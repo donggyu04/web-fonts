89ac1181f788a83306fe5f1b5d8447c8c4316a34
donggyu.lee <donggyu.lee@nts-corp.com>
2020-04-16 20:17:31 +0900

    Add skeleton

---
+++ src/main/java/com/training/fontdemo/FontDemoApplication.java
@@ -0,0 +1,13 @@
+package com.training.fontdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FontDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FontDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/training/fontdemo/FontDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.fontdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FontDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
